b2073f08ca221fd8cccd5280a392d7ca35c53348
HNINE <medhnine>
2025-07-20 20:30:20 +0100

    all

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,17 +1,53 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    """
+    Author model representing book authors.
+    """
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    """
+    Book model with ForeignKey relationship to Author.
+    """
+    title = models.CharField(max_length=300)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
+    
+    class Meta:
+        ordering = ['title']
 
 class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book, related_name="libraries")
+    """
+    Library model with ManyToMany relationship to Book.
+    """
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name='libraries')
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "Libraries"
 
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
-
+    """
+    Librarian model with OneToOne relationship to Library.
+    """
+    name = models.CharField(max_length=200)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    
+    def __str__(self):
+        return f"{self.name} (Librarian at {self.library.name})"
+    
+    class Meta:
+        ordering = ['name']
