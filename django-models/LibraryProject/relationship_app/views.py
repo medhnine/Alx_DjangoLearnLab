f64bf64de47cb822be1bf2114298e01194cc998b
HNINE <hninemed@gmail.com>
2025-07-25 01:23:22 +0100

    all done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -65,7 +65,7 @@ class ViewLibrary(DetailView):
     model = library
     template_name = 'relationship_app/library_detail.html'
 
-class RegisterView(CreateView):
+class UserCreationForm(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "relationship_app/register.html"
